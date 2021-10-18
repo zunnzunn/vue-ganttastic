3b93235b223ed3430eea33221a4573febf459d9c
Sergey Solodyagin <solodyagin@gmail.com>
2021-10-18 22:49:58 +0300

    fix: reduce build size

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,12 @@ module.exports = {
   outputDir: process.env.NODE_ENV === 'production' ? 'dist/' : 'demo/',
   css: { extract: false },
   productionSourceMap: false,
+  chainWebpack: config => {
+    if (process.env.VUE_CLI_BUILD_TARGET === 'lib') {
+      config.externals({
+        ...config.get('externals'),
+        moment: 'moment',
+      })
+    }
+  },
 }
