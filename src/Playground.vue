cebf747938127e0b5104bf2b98935d7cdc88caff
Gweltaz Niquel <gweltaz.niquel@externe.systeme-u.fr>
2021-11-24 16:02:07 +0100

    fetch mouvements

--- src/Playground.vue
+++ src/Playground.vue
@@ -14,14 +14,14 @@
         :theme="selectedTheme"
         @dragend-bar="onDragend($event)"
       >
-        <template v-for="row in rowList">
+        <template v-for="row in vehicules">
           <g-gantt-row 
             :key="row.label"
             :label="row.label"
-            :bars="row.barList"
+            :bars="row.mouvements"
             :highlight-on-hover="highlightOnHover"
-            bar-start="myStart"
-            bar-end="myEnd"
+            bar-start="mouvementDebut"
+            bar-end="mouvementFin"
           >
             <template #bar-label="{bar}">
               <span>{{bar.label}}</span>
@@ -35,6 +35,7 @@
 <script>
 import GGanttChart from './GGanttChart.vue'
 import GGanttRow from './GGanttRow.vue'
+import mouvementService from "./service/mouvementService";
 
 export default {
   components:{
@@ -71,105 +72,26 @@ export default {
         "fuchsia",
         "flare"
       ],
-      rowList: [
-        {
-          label: "Row #1",
-          barList: [
-            {
-              myStart: "2020-03-03 18:00",
-              myEnd: "2020-03-03 23:00",
-              label: "Immobile",
-              ganttBarConfig: {color:"white", backgroundColor: "#404040", opacity: 0.5, immobile: true}
-            },
-            {
-              myStart: "2020-03-03 04:00",
-              myEnd: "2020-03-03 15:00",
-              label: "Bar",
-              ganttBarConfig: {color:"white", backgroundColor: "#2e74a3", bundle: "blueBundle"}
-            }
-          ]
-        },
-
-        {
-          label: "Row #2",
-          barList: [
-            {
-              myStart: "2020-03-02 09:00",
-              myEnd: "2020-03-02 18:00",
-              image: "vue_ganttastic_logo_no_text.png",
-              label: "I have an image",
-              ganttBarConfig: {color:"white", backgroundColor: "#de3b26", bundle:"redBundle"}
-            },
-            {
-              myStart: "2020-03-03 04:00",
-              myEnd: "2020-03-03 15:00",
-              label: "We belong together ^",
-              ganttBarConfig: {color:"white", backgroundColor: "#2e74a3", bundle:"blueBundle"}
-            },
-            {
-              myStart: "2020-03-03 18:00",
-              myEnd: "2020-03-03 22:00",
-              label: "Bar",
-              ganttBarConfig: {color:"white", backgroundColor: "#aa34a3"}
-            }
-          ]
-        },
-
-        {
-          label: "Row #3",
-          barList: [
-            {
-              myStart: "2020-03-02 09:00",
-              myEnd: "2020-03-02 18:00",
-              label: "I am with stupid ^",
-              ganttBarConfig: {color:"white", backgroundColor: "#de3b26", bundle: "redBundle"}
-            },
-            {
-              myStart: "2020-03-02 22:30",
-              myEnd: "2020-03-03 05:00",
-              label: "With handles!",
-              ganttBarConfig: {color:"white", backgroundColor: "#a23def", handles: true}
-            },
-            {
-              myStart: "2020-03-02 01:00",
-              myEnd: "2020-03-02 07:00",
-              label: "Bar",
-              ganttBarConfig: {color:"white", backgroundColor: "#5effad", pushOnOverlap: false, zIndex: 3}
-            },
-            {
-              myStart: "2020-03-03 14:00",
-              myEnd: "2020-03-03 20:00",
-              label: "Woooow!",
-              ganttBarConfig: {color:"white", background: "repeating-linear-gradient(45deg,#de7359,#de7359 10px,#ffc803 10px,#ffc803 20px)"}
-            }, 
-          ]
-        },
-
-        {
-          label: "Row #4",
-          barList: [
-            {
-              myStart: "2020-03-03 06:30",
-              myEnd: "2020-03-03 20:00",
-              label: "Bar",
-              ganttBarConfig:{color:"white", backgroundColor: "#d18aaf", handles: true}
-            },
-            {
-              myStart: "2020-03-02 00:30",
-              myEnd: "2020-03-03 01:00",
-              label: "Rectangular",
-              ganttBarConfig: {color:"white", backgroundColor: "#f2840f", borderRadius: 0}
-            }, 
-          ]
-        }
-
-      ]
+      vehicules:[],
     }
   },
-
+  created : function() {
+    mouvementService.updateMouvements(this.vehicules)
+    this.onUpdate()
+  },
   methods: {
     onDragend(e){
       console.log(e)
+    },
+    onUpdate: function () {
+      this.vehicules.forEach(v => {
+        v.mouvements.forEach(m => {
+            m.ganttBarConfig = function () {
+              return {color: "white", backgroundColor: "blue", pushOnOverlap: true, handles: true}
+            }
+        })
+      })
+      console.log(this.vehicules)
     }
   }
 }
