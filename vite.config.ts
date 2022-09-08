c4b43dea5248371bdf43f8ad4684ea65ec41fbc0
Dominik Pschenitschni <mail@celement.de>
2022-09-08 14:30:27 +0200

    fix: remove favicon from built library

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import postcssPresetEnv from "postcss-preset-env"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: process.env.NODE_ENV === "production" ? "public" : false,
   css: {
     postcss: {
       plugins: [postcssPresetEnv()]
