bd5531171ce5369a032cf8d63bc1cfd256369309
Dominik Pschenitschni <mail@celement.de>
2022-09-08 13:15:11 +0200

    fix: remove vite-plugin-vue-type-imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import VueTypeImports from "vite-plugin-vue-type-imports"
 import postcssPresetEnv from "postcss-preset-env"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueTypeImports()],
+  plugins: [vue()],
   publicDir: process.env.NODE_ENV === "production" ? "public" : false,
   css: {
     postcss: {
