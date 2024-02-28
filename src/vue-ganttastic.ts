a7cac8823de721991319467e811cf7fd01cd80ae
PedroCanoas <pedro28canoas@gmail.com>
2024-02-28 19:51:41 -0300

    add-week-precision
    adding DyslexiaS's change from PR #20; adjusting week start of the year

--- src/vue-ganttastic.ts
+++ src/vue-ganttastic.ts
@@ -3,6 +3,8 @@ import dayjs from "dayjs"
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js"
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter.js"
 import isBetween from "dayjs/plugin/isBetween.js"
+import weekOfYear from "dayjs/plugin/weekOfYear"
+import advancedFormat from "dayjs/plugin/advancedFormat"
 import customParseFormat from "dayjs/plugin/customParseFormat.js"
 
 import type { GanttBarObject } from "./types.js"
@@ -16,6 +18,8 @@ export function extendDayjs() {
   dayjs.extend(isSameOrAfter)
   dayjs.extend(isBetween)
   dayjs.extend(customParseFormat)
+  dayjs.extend(weekOfYear)
+  dayjs.extend(advancedFormat)
 }
 
 export type { ColorScheme, GanttBarObject }
